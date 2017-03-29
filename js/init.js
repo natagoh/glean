58da9e8c755ca1cbc5ccb210b085d273e21ab376
natagoh <gohnatasha98@gmail.com>
2017-03-29 00:01:33 -0400

    added website files

---
+++ js/init.js
@@ -0,0 +1,24 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+// Disable Google Maps scrolling thanks to 
+// http://stackoverflow.com/questions/24768772/prevent-a-google-maps-iframe-from-capturing-the-mouses-scrolling-wheel-behavior
+$(document).ready(function () {
+    
+    // you want to enable the pointer events only on click;
+    $('.map-iframe').addClass('scrolloff'); // set the pointer events to none on doc ready
+    $('#map').on('click', function () {
+        $('.map-iframe').removeClass('scrolloff'); // set the pointer events true on click
+    });
+
+    // you want to disable pointer events when the mouse leave the canvas area;
+    $(".map-iframe").mouseleave(function () {
+        $('.map-iframe').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
+    });
+});
